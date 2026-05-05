7bd40be98de3e232ab07c627925d60d90cb0174f
Vladislav Tatjanin <l27001@ezdomain.ru>
2026-05-05 15:49:36 +0400

    Fix migration env.py to handle missing alembic.ini gracefully in Docker

--- migrations/env.py
+++ migrations/env.py
@@ -29,7 +29,10 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    try:
+        fileConfig(config.config_file_name)
+    except FileNotFoundError:
+        pass
 
 # add your model's MetaData object here
 # for 'autogenerate' support
