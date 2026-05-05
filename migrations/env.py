f9ed798fecaf96be4da76e1533f1ee5c283c713b
Vladislav Tatjanin <l27001@ezdomain.ru>
2026-05-05 15:57:25 +0400

    Fix migrations/env.py app context conflict causing AssertionError

--- migrations/env.py
+++ migrations/env.py
@@ -8,11 +8,10 @@ from alembic import context
 from dotenv import load_dotenv
 load_dotenv()
 
-from app import create_app, db
+from app import create_app
 
 # создаём Flask-приложение
 app = create_app()
-app.app_context().push()
 
 config = context.config
 
@@ -21,10 +20,6 @@ config.set_main_option(
     "sqlalchemy.url",
     app.config["SQLALCHEMY_DATABASE_URI"]
 )
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
