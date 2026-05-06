862e258bf04ca5d485f398cb26ea4ec1e2d5b1c5
Vladislav Tatjanin <l27001@ezdomain.ru>
2026-05-06 12:57:50 +0400

    add pool_pre_ping to reconnect stale database connections

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ class Config:
     # База данных
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+    }
 
     # JWT
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
